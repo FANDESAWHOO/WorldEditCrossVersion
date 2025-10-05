ae04a5bba681dffdb433ba38bfd9e9b0191b96e4
FANDESAWHOO <hcfpluginsv2@gmail.com>
2025-10-05 18:14:25 -0300

    working HEHEHEHE

--- src/main/java/crossversion/worldeditx/CrossVersion.java
+++ src/main/java/crossversion/worldeditx/CrossVersion.java
@@ -4,14 +4,19 @@ import org.mineacademy.fo.plugin.SimplePlugin;
 
 import crossversion.worldeditx.util.WorldEditAPIManager;
 import lombok.Getter;
-@Getter
+
 public class CrossVersion extends SimplePlugin {
-    private WorldEditAPIManager worldEditManager;
+	@Getter  public WorldEditAPIManager worldEditManager;
     
 	@Override
 	protected void onPluginStart() {
     worldEditManager = new WorldEditAPIManager();
+    worldEditManager.init();
 		
+	}
+	
+	public static CrossVersion getInstance() {
+		return (CrossVersion) SimplePlugin.getInstance();
 	}
 
 }
